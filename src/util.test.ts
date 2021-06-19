094504b8a3a3782005d971a722c29453b6599e36
John Ruble <john.ruble@atomicobject.com>
2021-06-19 00:40:05 -0400

    test for util

---
+++ src/util.test.ts
@@ -0,0 +1,16 @@
+import _ from "lodash";
+import { parseNumberEN, uuidv4 } from "./util";
+
+describe("Util", () => {
+  it("uuidv4() generates UUIDs", () => {
+    const a = uuidv4();
+    const b = uuidv4();
+    expect(a).not.toEqual(b);
+    expect(a.length).toEqual(36);
+  });
+
+  it("parseNumberEN()", () => {
+    expect(parseNumberEN("+123,4.56z")).toEqual(1234.56);
+    expect(parseNumberEN("-500.123")).toEqual(-500.123);
+  });
+});
